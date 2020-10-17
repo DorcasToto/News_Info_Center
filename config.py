cc883a9ae81c1ece1e9b3483455272f27194d6ea
Moringa School <dorcasmosonik@gmail.com>
2020-10-17 12:55:53 +0300

    classes

---
+++ config.py
@@ -0,0 +1,3 @@
+NEWS_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+
+NEWS_API_KEY = os.environ.get('MOVIE_API_KEY')
---
+++ start.sh
@@ -0,0 +1,1 @@
+export NEWS_API_KEY= '8c05912238f049e7baec7fd02050c3bc'
