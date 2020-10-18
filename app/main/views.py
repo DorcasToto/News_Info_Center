2fe79f0831681b8e86e031762c5cb5473e06ffff
Moringa School <dorcasmosonik@gmail.com>
2020-10-18 10:01:05 +0300

    display sources news

--- app/main/views.py
+++ app/main/views.py
@@ -7,7 +7,14 @@ from . import main
 def index():
 
     business = getSources('business')
+    entertainment = getSources('entertainment')
+    general = getSources('general')
+    health  = getSources('health')
+    science = getSources('science')
+    sports = getSources('sports')
+    technology = getSources('technology')
+    
 
     title = 'Home, Welcome to the best News Center'
 
-    return render_template('index.html',title = title, business = business)
+    return render_template('index.html',title = title, business = business,entertainment = entertainment, general = general, health = health,science = science, sports = sports,technology = technology)
